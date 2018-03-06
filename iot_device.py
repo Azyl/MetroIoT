fd60365dd92799c8738dc0c95042cedb424826be
Azyl <tataru.andrei.emanuel@gmail.com>
2018-03-06 16:41:42 +0200

    updated the class property

--- iot_device.py
+++ iot_device.py
@@ -16,8 +16,8 @@ class IOT_device(object):
 if __name__ == '__main__':
     device = IOT_device()
     print(device.iot_client.BASE_URL)
-    device.iot_client.jwt_token(None)
-    device.iot_client.jwt_iat(None)
+    device.iot_client.jwt_token = None
+    device.iot_client.jwt_iat = None
 
     for i in range(1, device.iot_client.num_messages+1):
         print(device.iot_client.jwt_token)
